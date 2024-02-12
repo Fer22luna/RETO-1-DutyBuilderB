62edca20fabe12b933b42f15c833ec8ecc14524e
Fernando G. Luna <fer22luna@gmail.com>
2024-02-12 19:15:11 -0300

    configurando la api de shyft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-ap.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal} from '@angular/core/rxjs-interop'
+import { computedAsync } from 'ngxtension/computed-async';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule, HdWalletMultiButtonComponent ],
   selector: 'duty-work-space-root',
-  templateUrl: './app.component.html',
+  template:`
+  <header>
+  <h1>hola</h1>
+  </header>
+  <div>
+
+  </div>
+  <hd-wallet-multi-button>
+ </hd-wallet-multi-button>
+
+  `,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'duty-work-space';
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync:true},
+  )
 }
